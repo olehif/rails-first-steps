44a91f97c3b56b3073da2e040004636281d31d22
oleh <oleh@i.ua>
2016-05-19 08:30:40 +0300

    no redister for admins

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,5 +2,5 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,:recoverable,
-         :rememberable, :registerable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
 end
