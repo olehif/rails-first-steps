8f0aec8a08b98ff70707aa19c5d774ec0b65a515
oleh <oleh@i.ua>
2016-05-19 21:56:26 +0300

    increase max title length

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,6 @@ class Question < ActiveRecord::Base
   has_many :fields
   has_many :votes
 
-  validates :title, length: {in: 2..20}
+  validates :title, length: {in: 2..200}
   validates :body,  length: {in: 1..1500}
 end
